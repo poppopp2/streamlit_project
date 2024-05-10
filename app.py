a9f27e673cb1e4af9ab40c548c9c4315144fc768
poppopp2 <166694558+poppopp2@users.noreply.github.com>
2024-05-10 10:05:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from userfile import run_userfile,save_uploaded_file
 
 def main():
     st.title('사용자가 업로드한 csv 파일을 분석합니다.')
-    st.subheader('사용자가 원하는 컬럼끼리의 상관관계, 여러 형태의 차트를 출력합니다!!')
+    st.subheader('사용자가 원하는 컬럼끼리의 상관관계, 여러 형태의 차트를 출력합니다.')
 
     run_userfile()
 
